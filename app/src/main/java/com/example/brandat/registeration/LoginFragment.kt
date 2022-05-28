4ca3e443bf2ea057da10e45081b9712641671057
elsheikh <az.galal666@gmail.com>
2022-05-28 16:50:44 -0700

    login and register screens UI

--- app/src/main/java/com/example/brandat/registeration/LoginFragment.kt
+++ app/src/main/java/com/example/brandat/registeration/LoginFragment.kt
@@ -1,24 +1,24 @@
-package com.example.brandat.ui.fragments.home
+package com.example.brandat.registeration
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-
 import com.example.brandat.R
-import com.example.brandat.registeration.RegistrationActivity
-import com.example.brandat.ui.ProductActivity
 
-class HomeFragment : Fragment() {
+class LoginFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
-
 }
