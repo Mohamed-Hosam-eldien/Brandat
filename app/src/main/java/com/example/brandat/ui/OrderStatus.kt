3392d5becd70ff135de90d61b4bcddd34d66727f
mohamedhosam <mohamedhosameldien07@gmail.com>
2022-05-28 21:16:21 +0200

    create ui for fragments on order status

--- app/src/main/java/com/example/brandat/ui/OrderStatus.kt
+++ app/src/main/java/com/example/brandat/ui/OrderStatus.kt
@@ -1,12 +1,40 @@
 package com.example.brandat.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.brandat.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.brandat.databinding.ActivityOrderStatusBinding
+
 
 class OrderStatus : AppCompatActivity() {
+
+    private lateinit var binding : ActivityOrderStatusBinding
+
+    private val stepList = arrayOf("Address","Payment Method","Verify")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_order_status)
+        binding = ActivityOrderStatusBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.stepView.statusView.setStatusList(stepList.toList())
+
+        binding.next.setOnClickListener{
+            if(binding.stepView.statusView.currentCount <= 2)
+                binding.stepView.statusView.currentCount = binding.stepView.statusView.currentCount + 1
+
+
+        }
+
+        binding.back.setOnClickListener{
+            if(binding.stepView.statusView.currentCount > 1)
+                binding.stepView.statusView.currentCount = binding.stepView.statusView.currentCount - 1
+        }
+
     }
+
+
+
+
+
 }
--- app/src/main/java/com/example/brandat/ui/Profile.kt
+++ app/src/main/java/com/example/brandat/ui/Profile.kt
@@ -2,29 +2,31 @@ package com.example.brandat.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.brandat.R
-import com.example.brandat.databinding.ActivityMainBinding
 import com.example.brandat.databinding.ActivityProfileBinding
 
 class Profile : AppCompatActivity() {
 
-
     private lateinit var binding: ActivityProfileBinding
-
-    private lateinit var navController: NavController
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-
+        initNavWithToolbar()
     }
+
+    private fun initNavWithToolbar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = getString(R.string.profile)
+        val navController = findNavController(R.id.navProfileHostFragment)
+        val appConfig = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController,appConfig)
+    }
+
 }
